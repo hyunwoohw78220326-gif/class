d428e09a8bd3248fb551fdbbeaac115949ec520f
hyunwoohw78220326-gif <hyunwoohw78220326@gmail.com>
2025-12-05 16:48:51 +0900

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,1 +1,9 @@
-
+firebaseConfig = {
+  "apiKey": "YOUR_API_KEY",
+  "authDomain": "YOUR_PROJECT_ID.firebaseapp.com",
+  "projectId": "class-18c72",
+  "storageBucket": "YOUR_PROJECT_ID.appspot.com",
+  "messagingSenderId": "YOUR_MSG_ID",
+  "appId": "1:490372450036:web:f7ad383c1d8f772ef480a1",
+  "databaseURL": ""
+}
